0546671d06893ce67f5ba4a735c8924da4a9dd12
AlphaDaniel <daniel.nunez.nyc@gmail.com>
2018-06-30 15:14:04 -0400

    Mechanize | +agent alias

--- lib/ruby_rails_helper/scrapers/rdo.rb
+++ lib/ruby_rails_helper/scrapers/rdo.rb
@@ -1,12 +1,15 @@
 module RubyRailsHelper
+  # Houses all scrapers used in this gem
   class Scraper
-    
+    # scrapes ruby-doc.org Core API
     def self.rdo_core
       agent = Mechanize.new
+      agent.user_agent_alias = 'Mac Safari'
+      
       page = agent.get('https://ruby-doc.org/core-2.5.1/')
       
       # in progress
-      page
+      binding.pry
     end
     
   end
