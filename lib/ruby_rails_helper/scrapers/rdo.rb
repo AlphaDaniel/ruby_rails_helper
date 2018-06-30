4bf0831ff76ccd925077f04390245532083a01dd
AlphaDaniel <daniel.nunez.nyc@gmail.com>
2018-06-29 20:04:14 -0400

    +rubocop dependency | +config

--- lib/ruby_rails_helper/scrapers/rdo.rb
+++ lib/ruby_rails_helper/scrapers/rdo.rb
@@ -1,12 +1,12 @@
 module RubyRailsHelper
   class Scraper
     
-    # scrapes ruby-doc.org Core API
     def self.rdo_core
       agent = Mechanize.new
       page = agent.get('https://ruby-doc.org/core-2.5.1/')
       
       # in progress
+      page
     end
     
   end
